669ac7a5c9bcff3f756ba2119fed705385f4cc5d
Alex <friedmanalexg@gmail.com>
2022-07-26 21:28:57 -0400

    drill 1 commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
+const listingsURL = "http://localhost:6001/listings"
+
 function App() {
+
+  const [listings, setListings] = useState([]) 
+
+  const removeListing = id => {
+    fetch (`${listingsURL}/${id}`,{
+      method: "DELETE" 
+    })
+      .then( () => {
+        const newListings = listings.filter(listingObj => listingObj.id !== id)
+        setListings(newListings)
+      })
+  }
+
+  const [search, setSearch]= useState('')
+
+  console.log(listings)
+  useEffect(() => {
+    fetch(listingsURL)
+      .then( r => r.json())
+      .then( listingsArray => setListings(listingsArray))
+  } ,[])
+
+  const submitSearch =someString => setSearch(someString)
+  
+
+  const searchedListings = listings.filter( listingObj => 
+    listingObj.description.toLowerCase().includes( search.toLowerCase())
+  )
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header submitSearch={submitSearch}/>
+      <ListingsContainer removeListing={removeListing} listings={searchedListings} />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,16 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({listings, removeListing}) {
+  
+  const listingComponents = listings.map( listing => {
+    return <ListingCard key={listing.id} listing = {listing} removeListing={removeListing}/>
+  })
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingComponents}
       </ul>
     </main>
   );
